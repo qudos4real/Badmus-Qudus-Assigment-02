fbeffa7ab2b562a3c561dfc71f25f125f7a9a2d9
Qudus Badmus <67230144+qudos4real@users.noreply.github.com>
2022-07-06 08:59:41 +0000

    Done challenge 1 and 2

--- src/assignment.js
+++ src/assignment.js
@@ -14,10 +14,15 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let sum = 0
+  for (let i = 0; i < arrayOfNumbers.length; i++) {
+    sum += arrayOfNumbers[i]
+  }
+  return sum
+    // return arrayOfNumbers.length;
+  // return (arrayOfNumbers.length)
 }
-// assignment.sumOfNumbers = sumOfNumbers;
-
+assignment.sumOfNumbers = sumOfNumbers;
 /**
  * Challenge - 2
  * 
@@ -28,9 +33,17 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let countOfEven = 0
+for (let j = 0; j<arrayOfNumbers.length; j++)
+if (arrayOfNumbers[j] % 2 == 0){
+  countOfEven += 1
+} else {
+  countOfEven += 0
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+return(countOfEven)
+    // return arrayOfNumbers.length;
+}
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
